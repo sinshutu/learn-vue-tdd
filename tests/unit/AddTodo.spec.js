ef44ef3f308f43db46087ba4814cffdfebe9cafc
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-09-10 01:12:01 +0900

    AddTodo.vueを作成

---
+++ tests/unit/AddTodo.spec.js
@@ -0,0 +1,20 @@
+import { shallowMount } from "@vue/test-utils";
+import AddTodo from "@/components/AddTodo.vue";
+
+describe("AddTodo.vue", () => {
+  it("input textを持っている", () => {
+    const wrapper = shallowMount(AddTodo);
+    expect(wrapper.find("input.add").exists()).toBe(true);
+  });
+  it("追加ボタンを押してemitする", () => {
+    const text = "todo";
+    const wrapper = shallowMount(AddTodo);
+    wrapper.setData({ text });
+    wrapper.find("input.add").trigger("keyup.enter");
+
+    // emitted の配列には
+    // emitted("Event Name")[emitが呼ばれた順番][引数]
+    // のような内容が入っている
+    expect(wrapper.emitted("addTodo")[0][0]).toBe(text);
+  });
+});
