1aea977be7bc96921783b2138fae0d61a41e6a7a
NaotoSuzuki <suzuki.naoto@teamup.jp>
2018-09-10 18:57:11 +0900

    TodoList.vueを作成

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <AddTodo />
+  </div>
+</template>
+
+<script>
+import AddTodo from "@/components/AddTodo.vue";
+
+export default {
+  name: "TodoList",
+  components: {
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    addTodo(text) {
+      this.todos.push({
+        id: this.todos.length + 1,
+        text: text,
+        checked: false
+      });
+    }
+  }
+};
+</script>
